c9d7d19b3f32694a4f193e9a1b3665655ba1b074
Artsiom Trubchyk <aglitchman@gmail.com>
2022-03-25 14:49:18 +0300

    Add support of Defold <1.3.1 + add info about game loop

--- scene3d/scripts/transform_interpolation.lua
+++ scene3d/scripts/transform_interpolation.lua
@@ -1,8 +1,50 @@
 --
--- Smoothly interpolated movement of your game objects
+-- Smoothly interpolated movement of your game objects.
 --
 -- Usage:
--- TODO :)
+-- ... WIP ...
+--
+
+-- Defold 1.3.1 and newer (https://forum.defold.com/t/defold-1-3-1-beta/70594).
+-- ^^^^^^^^^^^^^^^^^^^^^^
+--
+-- The `update` function is called on scripts every frame just after inputs are processed by
+-- Defold and before the screen is rendered. If `vsync` is disabled, when one frame is completed, 
+-- Defold will immediately start on the next one, therefore attaining the highest possible framerate.
+-- Since hardware and the complexity of each frame varies, the frame rate is never constant. Even
+-- with `vsync` enabled, causing Defold to try to match a specific frame rate, it is not truly
+-- constant. Because of this, `update` can be called any number of times per second.
+--
+-- The `fixed update` is called on scripts each time the physics simulation is progressed.
+-- `fixed update` and physics loop is synchronous, and does not occur on a separate thread.
+--
+-- The order of updates:
+--  - frame 1: update
+--  - frame 1: fixed update
+--  - frame 2: update
+--  - frame 2: fixed update
+--  - frame 3: update
+--  - frame 3: fixed update
+--  - frame 4: update
+--  - frame 5: update
+--  - frame 5: fixed update
+--  - frame 5: fixed update
+--  - frame 6: update
+--  - frame 6: fixed update
+--  - frame 7: update
+--  - frame 7: fixed update
+--  - frame 8: update
+--  - frame 8: fixed update
+--  - frame 9: update
+--  - frame 9: fixed update
+--  - frame 10: update
+--  - frame 10: fixed update
+--  - frame 11: update
+--  - frame 12: update
+--  - frame 12: fixed update
+--  - frame 12: fixed update
+--  - frame 13: update
+--  - frame 13: fixed update
 --
 
 local math3d = require("scene3d.helpers.math3d")
